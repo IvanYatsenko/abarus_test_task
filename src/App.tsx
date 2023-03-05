28ddaa018a682319766fd984ff1fefb8ba2cad50
yatsenko_i <yatsenko.ivan@rambler.ru>
2023-03-05 14:42:17 +0700

    установка axios и проверка api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+
 function App() {
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+  async function fetchPosts() {
+    try {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+      console.log(response.data)
+    } catch (error) {
+      alert(error)
+    }
+  }
   return (
     <div>
-     work!
+      work!
     </div>
   )
 }
